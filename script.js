9a66e90de1218c342c7244b1fdf7fcdcc72d9282
Navin Malakar <161839292+NavinMalakar2@users.noreply.github.com>
2025-07-10 16:04:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-	const heap = new MinHeap();
-
-  // Step 1: Insert all ropes into the heap
-  for (let rope of arr) {
-    heap.insert(rope);
-  }
-
+function mincost(arr) {
+  if (arr.length <= 1) return 0;
   let totalCost = 0;
-
-  // Step 2: While more than one rope is left, connect smallest two
-  while (heap.size() > 1) {
-    const first = heap.extractMin();
-    const second = heap.extractMin();
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+    const first = arr.shift();
+    const second = arr.shift();
     const cost = first + second;
     totalCost += cost;
-    heap.insert(cost);
+    arr.push(cost);
   }
-
   return totalCost;
-  
 }
 
-module.exports=mincost;
+module.exports = { mincost };
